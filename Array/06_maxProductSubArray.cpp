e77493e133deb62eac4c6b5efc3b0d28145f9c7e
ahmadrauf036 <ahmadrauf036@gmail.com>
2024-11-09 04:40:35 +0500

    modifications

--- Array/06_maxProductSubArray.cpp
+++ Array/06_maxProductSubArray.cpp
@@ -5,31 +5,27 @@
 #include<climits>
 using namespace std;
 
-int maxProduct(vector<int> nums){
-    int currProd=1,maxProd=INT_MIN,minProd=1;
-    for (size_t i = 0; i < nums.size(); i++)
-    {
-        currProd*=nums[i];
-        minProd*=currProd;
-        if(minProd<0){
-            if(minProd>maxProd){
-                maxProd=minProd;
-            }
-            currProd=1;
-
-        }
-        else if(maxProd<minProd){
-            maxProd=currProd;
-        }
-        if(currProd==0)
-        {
-            currProd=1;
-            minProd=1;
+int maxProduct(vector<int>& nums) {
+    int currProd = 1, maxProd = INT_MIN, minProd = 1;
+    
+    for (size_t i = 0; i < nums.size(); i++) {
+        if (nums[i] < 0) {
+            swap(currProd, minProd);
         }
         
+        currProd = max(nums[i], currProd * nums[i]);
+        minProd = min(nums[i], minProd * nums[i]);
+
+        maxProd = max(maxProd, currProd);
+        
+        if (nums[i] == 0) {
+            currProd = 1;
+            minProd = 1;
+        }
     }
     return maxProd;
 }
+
 
 int main(){
     vector<int> nums1={-2,1,-3,4,-1,2,1,-5,4};
--- Array/09_3Sum.cpp
+++ Array/09_3Sum.cpp
@@ -1,101 +1,57 @@
 #include<iostream>
 #include<vector>
-#include<unordered_map>
 #include <algorithm>
 using namespace std;
 
-vector<vector<int>> two_sum(vector<int> nums,int target,int ignore=-1)
-{
-    
-    unordered_map<int,int> m;
-    vector<vector<int>> fresult={};
-    vector<int> result={};
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if(i!=ignore){
-            m[nums[i]]=i;
-        }
-    }
-    for(int i=0;i<nums.size();i++)
-    {
-        if(ignore!=i){
-            auto found=m.find(target-nums[i]);
-            if(found!=m.end())
-            {
-                if(found->second!=i)
-                {
-                    result.push_back(nums[i]);
-                    result.push_back(found->first);
-                    result.push_back(target*-1);
-                    fresult.push_back(result);
-                }
+vector<vector<int>> threeSum(vector<int> nums) {
+    vector<vector<int>> finalResult;
+    sort(nums.begin(), nums.end()); 
+
+    for (int i = 0; i < nums.size() - 2; i++) {
+        
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        int st = i + 1, end = nums.size() - 1;
+        while (st < end) {
+            int sum = nums[i] + nums[st] + nums[end];
+            if (sum == 0) {
+                finalResult.push_back({ nums[i], nums[st], nums[end] });
+                while (st < end && nums[st] == nums[st + 1]) st++;
+                while (st < end && nums[end] == nums[end - 1]) end--;
+
+                st++;
+                end--;
+            }
+            else if (sum < 0) {
+                st++;
+            }
+            else {
+                end--;
             }
         }
     }
-    return fresult;
+    return finalResult;
 }
 
-vector<vector<int>> threeSum(vector<int> nums){
-    unordered_map<int,int> mp1;
-    vector<vector<int>> result={};
-    vector<vector<int>> finalResult={};
+int main() {
+    vector<int> nums = { 1,2,3,0,-1 };
+    vector<vector<int>> result = threeSum(nums);
 
-    for (size_t i = 0; i < nums.size(); i++){
-            mp1[i]=nums[i]*(-1);
-    }
-    for (auto i:mp1){
-        result=two_sum(nums,i.second,i.first);
-        if(result.empty()){
-            continue;
-        }
-        bool found=0;
-        if(!finalResult.empty()){
-            for (size_t i = 0; i < finalResult.size(); i++)
-            {
-                for(size_t j = 0; j < finalResult.size(); j++){
-                    sort(result[j].begin(),result[j].end());
-                    if(finalResult[i]==result[j]){
-                        found=1;
-                        continue;
-                    }
-                    finalResult.push_back(result[j]);
-                }
-            }
-        }
-        else{
-            for(size_t j = 0; j < finalResult.size(); j++){
-                    sort(result[j].begin(),result[j].end());
-                    finalResult.push_back(result[j]);
-                }
-        }
-        if(found){
-            continue;
-        }
-    }
-    return finalResult;    
-    
-}
-
-int main(){ 
-    vector<int> nums={3,0,-2,-1,1,2};
-    vector<vector<int>> result=threeSum(nums);
-    
-    if(result.empty()){
-        cout<<"No such triplet exists.";
+    if (result.empty()) {
+        cout << "No such triplet exists.";
     }
     for (size_t i = 0; i < result.size(); i++)
     {
-        cout<<"[";
+        cout << "[";
         for (size_t j = 0; j < result[i].size(); j++)
         {
-            cout<<result[i][j];
-            if(j!=result[i].size()-1)
+            cout << result[i][j];
+            if (j != result[i].size() - 1)
             {
-                cout<<", ";
+                cout << ", ";
             }
         }
-        cout<<"]"<<endl;
-        
+        cout << "]" << endl;
     }
-    
+    return 0;
 }
